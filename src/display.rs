7d600c4bbc44d765bc3cec98072f0bff89151476
Anton Johansson <antojohansson1@gmail.com>
2023-03-23 23:42:25 +0100

    a

--- src/display.rs
+++ src/display.rs
@@ -7,6 +7,20 @@ use termion::raw::IntoRawMode;
 use std::io::{Write, stdout, stdin};
 
 
+
+///Draw to the terminal, one line at a time
+fn draw(file: &FileObject, stdout: &mut std::io::Stdout, pos: &Position) {
+    write!(stdout, "{}", termion::cursor::Goto(1, 1)).unwrap();
+    for i in 0..file.len() {
+        write!(stdout, "{}\r\n", file.get_line(i)).unwrap();
+    }
+    write!(stdout, "{}", termion::cursor::Goto(pos.x, pos.y)).unwrap();
+    stdout.flush().unwrap();
+}
+
+///This is function which initilizes everything from file to position.
+///It also handles the keypresses and calls the draw function.
+///This is good for the moment but should be split up into a more modular design
 pub fn init() {
 
     let mut pos = Position { x: 1, y: 1 };
@@ -36,12 +50,29 @@ pub fn init() {
                         .unwrap();
                 stdout.flush().unwrap();
                
-                for i in 0..file.len() {
-                    write!(stdout, "{}{}\r", file.get_line(i), file.get_line(i).len()).unwrap();
-                }
+                draw(&file, &mut stdout, &pos);
+            }
+            Key::Ctrl('c') => break,
+            Key::Up => {
+                pos = file.move_cursor(0, -1);
                 write!(stdout, "{}", termion::cursor::Goto(pos.x, pos.y)).unwrap();
                 stdout.flush().unwrap();
-            }
+            },
+            Key::Down => {
+                pos = file.move_cursor(0, 1);
+                write!(stdout, "{}", termion::cursor::Goto(pos.x, pos.y)).unwrap();
+                stdout.flush().unwrap();
+            },
+            Key::Left => {
+                pos = file.move_cursor(-1, 0);
+                write!(stdout, "{}", termion::cursor::Goto(pos.x, pos.y)).unwrap();
+                stdout.flush().unwrap();
+            },
+            Key::Right => {
+                pos = file.move_cursor(1, 0);
+                write!(stdout, "{}", termion::cursor::Goto(pos.x, pos.y)).unwrap();
+                stdout.flush().unwrap();
+            },
             _ => ()
         }
     }
--- src/file.rs
+++ src/file.rs
@@ -1,5 +1,9 @@
 use super::position::Position;
 
+
+///This is the object which holds the line data
+///It is a vector of chars in order to add into any position
+///Having a string causes it to crash while inserting UTF-8 at the wrong offset. 
 struct Line {
     text: Vec<char>,
 }
@@ -9,12 +13,15 @@ impl Line {
     fn new() -> Line {
         Line { text: Vec::new() }
     }
-
+    
+    ///Add a char to the line at the given position
     fn add_char(&mut self, c: char, x: u32) {
         self.text.insert(x as usize, c);
     }
 }
 
+///This is the object which holds the file data
+///This like other objects is at the moment a bit simple and could be made better. 
 pub struct FileObject {
     path: String,
     lines: Vec<Line>,
@@ -33,13 +40,12 @@ impl FileObject {
             
     }
 
-    
+    ///Adds a character to the file at the current position of the cursor
     pub fn add_char(&mut self, c: char) -> Position {
         match c {
             '\n' => {
                 match self.lines.get_mut(self.position.y as usize) {
-                    Some(line) => {
-                        line.add_char(c, self.position.x as u32);
+                    Some(_) => {
                         self.position.x = 0;
                         self.position.y += 1;
                         self.lines.insert(self.position.y as usize, Line::new());
@@ -60,12 +66,13 @@ impl FileObject {
                 }
             }
         }
-        let mut s = self.position.clone();
-        s.x += 1;
-        s.y += 1;
-        s
+        self.get_cords()
     }
     
+    pub fn get_cords(&self) -> Position {
+        Position { x: self.position.x + 1, y: self.position.y + 1 }
+    }
+
     pub fn len(&self) -> u16 {
         self.lines.len() as u16
     }
@@ -75,6 +82,38 @@ impl FileObject {
             Some(line) => line.text.iter().collect(),
             None => "".to_string()
         }
+    }
+ 
+
+    ///This is the function which is called to move the cursor to different positions in the file.
+    ///It takes two arguments, x and y, which are the amount of characters to move the cursor in the x and y direction.
+    ///It returns a Position struct which contains the new x and y coordinates of the cursor.
+    ///It also guards against moving the cursor to disallowed positions.
+    pub fn move_cursor(&mut self, x: i32, y: i32) -> Position {
+        let mut x_temp: i32 = self.position.x as i32 + x;
+        let mut y_temp: i32 = self.position.y as i32 + y;
+        
+        //Correct the y-axis
+        while y_temp >= self.lines.len() as i32 {
+            self.lines.insert(self.lines.len(), Line::new());
+        }
+        if y_temp < 0 {
+            y_temp = 0;
+        }
+        self.position.y = y_temp as u16;
+
+        //Correct the x-axis
+        if x_temp > self.lines[self.position.y as usize].text.len() as i32 {
+            x_temp = self.lines[self.position.y  as usize].text.len() as i32;
+        }
+
+        if x_temp < 0 {
+            x_temp = 0;
+        }
+
+        self.position.x = x_temp as u16;
+        
+        self.get_cords()
     }
 
 }
